bcdee17ec69362b95e65fcfd6119d3df80c98fbd
alen james <alenjames899@gmail.com>
2025-12-02 13:35:47 +0530

    navbar

--- app/webdesigning/page.jsx
+++ app/webdesigning/page.jsx
@@ -83,7 +83,13 @@ export default function Page() {
 >
   <Link
     href="/contact"
-    className=" inline-block px-7 py-4 bg-[#6c53a7] text-white rounded-xl shadow-lg hover:bg-[#8d4368] transition-all momo-font"
+    className=" inline-block px-7 py-4   text-white text-sm momo-font rounded-lg
+                bg-gradient-to-r from-[#ae5c83] to-[#5b4390]
+                shadow-[0_10px_25px_rgba(174,92,131,0.35)]
+                hover:shadow-[0_15px_30px_rgba(91,67,144,0.45)]
+                hover:scale-[1.06]
+                transition-all duration-300 ease-out
+                backdrop-blur-md"
   >
     Schedule an Intro Call
   </Link>
